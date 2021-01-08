711fc655df20c6661794b1cb5d9fc27b112fe50b
Jakub Kozera <kozerakuba@gmail.com>
2021-01-08 07:33:12 +0100

    1.5 Wstrzykiwanie zależności

---
+++ RestaurantAPI/Controllers/WeatherForecastController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RestaurantAPI.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+
+        private readonly IWeatherForecastService _service;
+        private readonly ILogger<WeatherForecastController> _logger;
+
+        public WeatherForecastController(ILogger<WeatherForecastController> logger, IWeatherForecastService service)
+        {
+            _logger = logger;
+            _service = service;
+        }
+
+        [HttpGet]
+        public IEnumerable<WeatherForecast> Get()
+        {
+            var result = _service.Get();
+            return result;
+        }
+    }
+}
---
+++ RestaurantAPI/IWeatherForecastService.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections.Generic;
+
+namespace RestaurantAPI
+{
+    public interface IWeatherForecastService
+    {
+        IEnumerable<WeatherForecast> Get();
+    }
+}
