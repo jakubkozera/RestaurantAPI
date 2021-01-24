b7111dae14d7f25bbb87a8c0df2304239c671575
Jakub Kozera <kozerakuba@gmail.com>
2021-01-24 09:56:00 +0100

    5.3 Usuwanie subencji

--- RestaurantAPI/Services/DishService.cs
+++ RestaurantAPI/Services/DishService.cs
@@ -16,6 +16,7 @@ namespace RestaurantAPI.Services
         int Create(int restaurantId, CreateDishDto dto);
         DishDto GetById(int restaurantId, int dishId);
         List<DishDto> GetAll(int restaurantId);
+        void RemoveAll(int restaurantId);
     }
 
     public class DishService : IDishService
@@ -30,10 +31,7 @@ namespace RestaurantAPI.Services
         }
         public int Create(int restaurantId, CreateDishDto dto)
         {
-            var restaurant = _context.Restaurants.FirstOrDefault(r => r.Id == restaurantId);
-            if(restaurant is null) 
-                throw new NotFoundException("Restaurant not found");
-
+            var restaurant = GetRestaurantById(restaurantId);
             var dishEntity = _mapper.Map<Dish>(dto);
 
             dishEntity.RestaurantId = restaurantId;
@@ -46,9 +44,7 @@ namespace RestaurantAPI.Services
 
         public DishDto GetById(int restaurantId, int dishId)
         {
-            var restaurant = _context.Restaurants.FirstOrDefault(r => r.Id == restaurantId);
-            if (restaurant is null)
-                throw new NotFoundException("Restaurant not found");
+            var restaurant = GetRestaurantById(restaurantId);
 
             var dish = _context.Dishes.FirstOrDefault(d => d.Id == dishId);
             if (dish is null || dish.RestaurantId != restaurantId)
@@ -62,6 +58,24 @@ namespace RestaurantAPI.Services
 
         public List<DishDto> GetAll(int restaurantId)
         {
+
+            var restaurant = GetRestaurantById(restaurantId);
+            var dishDtos = _mapper.Map<List<DishDto>>(restaurant.Dishes);
+
+            return dishDtos;
+        }
+
+        public void RemoveAll(int restaurantId)
+        {
+            var restaurant = GetRestaurantById(restaurantId);
+
+            _context.RemoveRange(restaurant.Dishes);
+            _context.SaveChanges();
+
+        }
+
+        private Restaurant GetRestaurantById(int restaurantId)
+        {
             var restaurant = _context
                 .Restaurants
                 .Include(r => r.Dishes)
@@ -70,9 +84,7 @@ namespace RestaurantAPI.Services
             if (restaurant is null)
                 throw new NotFoundException("Restaurant not found");
 
-            var dishDtos = _mapper.Map<List<DishDto>>(restaurant.Dishes);
-
-            return dishDtos;
+            return restaurant;
         }
     }
 }
