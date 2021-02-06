d428effff88f2f9df81fb82c906ad06c0ae175a7
Jakub Kozera <kozerakuba@gmail.com>
2021-02-06 12:36:51 +0100

    9.1 CORS

--- RestaurantAPI/Startup.cs
+++ RestaurantAPI/Startup.cs
@@ -85,11 +85,22 @@ namespace RestaurantAPI
             services.AddScoped<IUserContextService, UserContextService>();
             services.AddHttpContextAccessor();
             services.AddSwaggerGen();
+            services.AddCors(options =>
+            {
+                options.AddPolicy("FrontEndClient", builder => 
+                    
+                    builder.AllowAnyMethod()
+                        .AllowAnyHeader()
+                        .WithOrigins(Configuration["AllowedOrigins"])
+                    
+                    );
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env, RestaurantSeeder seeder)
         {
+            app.UseCors("FrontEndClient");
             seeder.Seed();
             if (env.IsDevelopment())
             {
