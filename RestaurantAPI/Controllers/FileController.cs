2069ed2cfcaf278cc881f53bafd2e71687e34a45
Jakub Kozera <kozerakuba@gmail.com>
2021-02-07 08:22:45 +0100

    9.2 Zwracanie plików statycznych

---
+++ RestaurantAPI/Controllers/FileController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.StaticFiles;
+
+namespace RestaurantAPI.Controllers
+{
+    [Route("file")]
+    [Authorize]
+    public class FileController : ControllerBase
+    {
+        public ActionResult GetFile([FromQuery] string fileName)
+        {
+            var rootPath = Directory.GetCurrentDirectory();
+
+            var filePath = $"{rootPath}/PrivateFiles/{fileName}";
+
+            var fileExists = System.IO.File.Exists(filePath);
+            if (!fileExists)
+            {
+                return NotFound();
+            }
+
+            var contentProvider = new FileExtensionContentTypeProvider();
+            contentProvider.TryGetContentType(fileName, out string contentType);
+
+            var fileContents = System.IO.File.ReadAllBytes(filePath);
+
+            return File(fileContents, contentType, fileName);
+        }
+    }
+}
--- RestaurantAPI/Startup.cs
+++ RestaurantAPI/Startup.cs
@@ -100,6 +100,7 @@ namespace RestaurantAPI
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env, RestaurantSeeder seeder)
         {
+            app.UseStaticFiles();
             app.UseCors("FrontEndClient");
             seeder.Seed();
             if (env.IsDevelopment())
