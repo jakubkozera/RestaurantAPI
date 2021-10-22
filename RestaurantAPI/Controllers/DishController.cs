a8c1fe68a9da7259203cf782a9f13d2cf222f68e
PC\Jakub <jakubkozera@outlook.com>
2021-10-22 08:10:06 +0200

    Authorize

--- RestaurantAPI/Controllers/DishController.cs
+++ RestaurantAPI/Controllers/DishController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using RestaurantAPI.Models;
 using RestaurantAPI.Services;
@@ -10,6 +11,7 @@ using RestaurantAPI.Services;
 namespace RestaurantAPI.Controllers
 {
     [Route("api/restaurant/{restaurantId}/dish")]
+    [Authorize]
     [ApiController]
     public class DishController : ControllerBase
     {
